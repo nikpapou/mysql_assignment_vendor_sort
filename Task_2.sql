d0485780376014ef298575285bf6c3bc47b8e196
Nikolaos Papountzas <157731120+nikpapou@users.noreply.github.com>
2024-12-21 18:54:32 +0200

    Delete Task_2.sql

--- Task_2.sql
+++
@@ -1,7 +0,0 @@
-SELECT d.first_name, d.last_name, d.wealth_segment, a.property_valuation
-FROM reatcodeltd_axldp_Customer_Demographic d
-JOIN reatcodeltd_axldp_Customer_Address a
-ON d.customer_id = a.customer_id
-WHERE wealth_segment = "Mass Customer"
-HAVING property_valuation > 5
-LIMIT 10;
